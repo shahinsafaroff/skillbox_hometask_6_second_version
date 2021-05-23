008cd73db31919e761515bee450e80463dc895f8
Shahin Safaroff <shahinsafaroff@gmail.com>
2021-05-24 01:51:00 +0400

    Final revise

--- skillbox_hometask_6.3/main.go
+++ skillbox_hometask_6.3/main.go
@@ -2,14 +2,20 @@ package main
 import "fmt"
 func main() {
 	var price, discount float64
+
 	fmt.Println("The Original Price of item:  ")
 	fmt.Scan(&price)
+
 	fmt.Println("Discount for this item: ")
 	fmt.Scan(&discount)
 
-		if (discount == price*0.3 && discount <= 2000) {
-			fmt.Println("Here is final Discount Amount: ", discount)
-		} else {
-			fmt.Println("Discount is too much!!!")
-		}
+	for discount > price*0.3 || discount > 2000 {
+		fmt.Println("Discount is too much!!!")
+		fmt.Println("Discount for this item: ")
+		fmt.Scan(&discount)
+	}
+
+	fmt.Println("Price:", price)
+	fmt.Println("Discount", discount)
+	fmt.Println("Total:", price-discount)
 	}
